f21fc565140c996426b672b64fab7b82100aeec5
dmlyrae <dm.lyrae@ya.ru>
2020-12-08 18:38:47 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    width: 438,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.arrow',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        595: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+let menu = document.querySelector('.menu-mobile');
+let menuButton = document.querySelector('.menu-button');
+let header = document.querySelector('.header');
+menu.addEventListener ('click', (event) => {
+    header.classList.toggle('header-active');
+    menuButton.classList.toggle('menu-button-active');
+})
