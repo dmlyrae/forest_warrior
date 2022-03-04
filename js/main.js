13e12f4714ec9a4b446127f661f1e998d436dac1
DM Lyrae <dm.lyrae@ya.ru>
2022-03-04 14:15:35 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    width: 438,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.arrow',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        595: {
-            slidesPerView: 2,
+window.onload = function() {
+    let mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        width: 438,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.arrow',
+            prevEl: '.swiper-button-prev',
         },
-    },
-});
+        breakpoints: {
+            595: {
+                slidesPerView: 2,
+            },
+        },
+    });
 
-let menu = document.querySelector('.menu-mobile');
-let menuButton = document.querySelector('.menu-button');
-let header = document.querySelector('.header');
-menu.addEventListener ('click', (event) => {
-    header.classList.toggle('header-active');
-    menuButton.classList.toggle('menu-button-active');
-})
+    let menu = document.querySelector('.menu-mobile');
+    let menuButton = document.querySelector('.menu-button');
+    let header = document.querySelector('.header');
+    menu.addEventListener ('click', (event) => {
+        header.classList.toggle('header-active');
+        menuButton.classList.toggle('menu-button-active');
+    })
+}
